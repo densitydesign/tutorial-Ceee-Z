f03a272cf77c1bd3978462494462d1a9607ebdf9
Ceee-Z <118460602+Ceee-Z@users.noreply.github.com>
2022-11-24 12:46:49 +0100

    Uploading try

---
+++ script.js
@@ -0,0 +1,26 @@
+let data = d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRRy2dEW3gKEE-oWeDQUG3awj9-QFWm5XXUeTELWG3rILfFRrkEDglmGgTOebVuD8LY5Q8QihNvgU3H/pub?gid=0&single=true&output=csv");
+
+let cardContainer = d3.select("#visualization-container");
+
+data.then(function(myData){
+    console.log(myData);
+
+    let card = cardContainer.selectAll("div")
+    .data(myData)
+    .join("div")
+    .attr("class","card")
+
+    card.append("div")
+    .attr("class","card-header")
+    .text(function(datum){
+        return datum.timestamp;
+    });
+
+    card.append("img")
+    .attr("class","card-img-top")
+    .attr("scr",function(datum){
+        let path = ("assets/images/");
+        let filename = datum.filename;
+        return path + filename;
+    });
+});
